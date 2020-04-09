657600c61d58629f14d2727321ad1f71161ccb96
DroidApple <31959510+DroidApple@users.noreply.github.com>
2020-04-09 23:51:14 +0100

    Update install_exchange.php

--- install_exchange.php
+++ install_exchange.php
@@ -1,1 +1,281 @@
 
+
+
+<body class="fix-header fix-sidebar card-no-border">
+
+
+<div id="main-wrapper">
+
+<div style="max-width: 800px; margin: 0 auto;">
+        <div class="scroll-sidebar">
+
+
+            <nav class="sidebar-nav stickyside" style="active none;">
+                <ul id="sidebarnav">
+                   
+
+                    <li><a class="waves-effect waves-dark" href="#accordion-content1"
+                           aria-expanded="false">Welcome to DroidApple.com Terms and Policies</a>
+
+                    </li>
+                    <li><a class="waves-effect waves-dark" href="#accordion-content2"
+                           aria-expanded="false">Using </a>
+
+                    </li>
+                    
+                    <li><a class="waves-effect waves-dark" href="#accordion-content4"
+                           aria-expanded="false">  Content</a>
+                    </li>
+                   
+                  
+                    <li><a class="waves-effect waves-dark" href="#accordion-content7" aria-expanded="false">Our
+                            Warranties and Disclaimers</a>
+                    </li>
+                    <li><a class="waves-effect waves-dark" href="#accordion-content8" aria-expanded="false">Liability
+                            for our Service</a>
+                    </li>
+                    <li><a class="waves-effect waves-dark" href="#accordion-content9" aria-expanded="false">About these
+                            Terms </a>
+                    </li>
+                   
+                    <li><a class="waves-effect waves-dark" href="#accordion-content11"
+                           aria-expanded="false">Binding Agreement</a></li>
+                    <li><a class="waves-effect waves-dark" href="#accordion-content12"
+                           aria-expanded="false">Privacy Policy</a></li>
+                   
+                    <li><a class="waves-effect waves-dark" href="#accordion-content19"
+                           aria-expanded="false">GDPR</a></li>
+
+                    <li><a class="waves-effect waves-dark" href="#accordion-content20"
+                           aria-expanded="false">Definitions</a></li>
+
+
+                </ul>
+            </nav>
+
+        </div>
+</div>
+    
+
+    <div class="page-wrapper">
+
+
+        <div class="container-fluid">
+
+            <div style="max-width: 800px; margin: 0 auto;">
+                <div class="card">
+                    <div class="card-body">
+                        <h1>Terms of Service</h1>
+                        <hr>
+                        <div id="accordion-content1">
+                            <h3 class="accordion-toggle">Welcome to DroidApple &#9662</h3>
+
+                            <p>
+
+                                <strong>These Terms are effective as of 2020/01/01.</strong><br>
+                                We may revise these Terms at any time by updating this page, changes will be binding on
+                                you on the date they are updated.<br><br>
+
+                                By using our GAMES, you are accepting and agreeing to be bound by these
+                                terms.<br>
+
+
+                            </p>
+                        </div>
+
+                        <div id="accordion-content2">
+                            <h3 class="accordion-toggle">Using &#9662</h3>
+                            <p>
+
+
+                                You must follow any Terms and Policies made available to you<br>
+                                It is your responsibility to ensure that your use of complies
+                                with these
+                                Terms and Policies.<br>
+                            
+                                Using our GAMES does not give you ownership of any intellectual property rights. <br>
+                                These terms do not grant you the right to use any branding or logos used
+                                in our GAMES.<br>
+
+
+                            </p>
+                        </div>
+ 
+
+                            <div id="accordion-content4">
+                                <h3 class="accordion-toggle"> Content &#9662</h3>
+                                <p>
+
+                                    We own all the contents that we have put on  our GAMES.
+                                    This includes the design, compilation, and look and feel,
+                                    and copyright, trademarks, designs and other intellectual
+                                    property on  .<br>
+                                  
+                                </p>
+                            </div>
+
+                          
+                            <div id="accordion-content7">
+                                <h3 class="accordion-toggle"> Our Warranties and Disclaimers &#9662</h3>
+
+                                <p>
+
+                                    We provide GAMEs.
+                                    We hope that you will find our GAMES, interesting.<br>
+                                    But there are certain things that we don’t promise.
+                                    OTHER THAN AS EXPRESSLY SET OUT IN THESE TERMS OR ADDITIONAL TERMS, WE DOT NOT MAKE
+                                    ANY SPECIFIC PROMISES ABOUT THE GAMES FUNCTIOLALITIES.<br>
+                                    WE PROVIDE THE GAME “AS IS”.
+
+
+                                </p>
+                            </div>
+                            <div id="accordion-content8">
+                                <h3 class="accordion-toggle"> Liability &#9662</h3>
+
+                                <p>
+
+                                    IN ANY CASE (WHATEVER) WE WILL NOT BE LIABLE TO YOU OR ANY OTHER PERSON FOR LOST
+                                    COST OF ANY KIND IN CONNECTION WITH THIS AGREEMENT,
+                                    OR FOR LOST COST OF ANY KIND ARISING OUT OF OR RELATING TO THIS AGREEMENT, EVEN IF
+                                    WE HAS BEEN ADVISED OF THE POSSIBILITY OF THOSE COSTS.
+                                    FURTHER, WE <strong>WILL NOT BE LIABLE TO YOU</strong> IN CONNECTION WITH THE
+                                    PARTICULAR GAME GIVING RISE TO THE CLAIM.
+
+                                </p>
+                            </div>
+                            <div id="accordion-content9">
+                                <h3 class="accordion-toggle"> About these Terms &#9662</h3>
+
+                                <p>
+
+                                    We may modify these terms or any additional terms that apply to this Service.
+                                    <strong>You should look at the terms regularly, your use of  
+                                        indicates your acceptance of the modified Terms.</strong>.<br>
+                                    <strong>If you do not agree to the modified terms, you should discontinue your use
+                                        of the
+                                        Service.</strong><br>
+                                    These terms control the relationship between <strong>Us and You.</strong> They do
+                                    create third party beneficiary rights.
+                                    If you do not comply with these terms, and we don’t take action right away, this
+                                    doesn't mean that we are giving up any rights that we may have (such as taking action in the future).<br>
+                                    If it turns out that a particular term is not enforceable, this will not affect any
+                                    other terms.
+
+                                </p>
+                            </div>
+                            
+
+                            <div id="accordion-content11">
+                                <h3 class="accordion-toggle"> Binding Agreement &#9662</h3>
+
+                                <p>
+
+                                    These Terms form a binding agreement (the "Agreement") between You and US.<br>
+                                    Your use of our GAMES indicates your acceptance of these Terms.
+                                    <strong>And you are accepting to play our GAMES at your own risk.</strong>
+
+                                </p>
+                            </div>
+
+
+                            <div id="accordion-content12">
+                                <h1>Privacy Policy</h1>
+                                <hr>
+                                <h3 class="accordion-toggle"> Welcome to the Privacy Policy
+                                    &#9662</h3>
+                                <p>
+
+                                    When you use our GAMES, you trust us with your information.
+                                    This Privacy Policy is meant to help you understand what <strong>data we
+                                        collect,</strong><br>
+									<strong>Please be adviced we do not knowingly collect or solicit information data (personal information, location etc...) from you,</strong>
+									Google: https://developers.google.com/games/services/terms
+									Chartboost: https://answers.chartboost.com/en-us/articles/200780239
+                                    Those third-party they collect it, and what they do with it. This is important; we hope you will take
+                                    time to read it carefully.
+
+                                </p>
+                            </div>
+                         
+
+                            <div id="accordion-content19">
+                                <h3 class="accordion-toggle">GDPR: General Data Protection Regulation</h3>
+
+                                <p>
+We do not knowingly collect or solicit information data (personal information, location etc...) from you, but we do use third-party and ad networks:
+Please check those links:
+Google game service: https://developers.google.com/games/services/terms
+Chartboost: https://answers.chartboost.com/en-us/articles/200780239
+Those may collect and process personal data such as device identifiers, location data, and other demographic and interest data to provide advertising experience tailored to you. You can disable personalization of advertising at any time by going into the settings of google settings in your phone.
+
+
+                                </p>
+
+                            </div>
+
+
+                            <div id="accordion-content20">
+                                <h1>Definitions</h1>
+                                <hr>
+                                <h3>DroidApple, We, Us:</h3>
+
+                                <p>
+                                    Refers to owner <a href="https://droidapple.github.io">DroidApple</a><br>
+                                    The web site and the Android mobile Apps and Games.
+
+                                </p>
+                                <h3>You:</h3>
+
+                                <p>
+                                    Refers to a user that is accepting these Terms.
+                                </p>
+                                
+                                <h3>Personal information:</h3>
+
+                                <p>
+                                    Refers to information that personally identifies you, such as your name, address,
+                                    telephone number, email address, or other information.
+                                </p>
+                               
+
+                                <h3>  content:</h3>
+
+                                <p>
+
+                                    All the content that we use in our GAMES includes the design,
+                                    compilation, and look
+                                    and feel of the  , and copyright, trademarks, designs and
+                                    other
+                                    intellectual property on
+                                    the.
+
+
+                                </p>
+
+
+                                <h3>GAMES</h3>
+
+                                <p>
+
+                                    Our games we put in google play store.
+
+                                </p>
+                            </div>
+
+
+                        </div>
+
+
+                    </div>
+
+
+                    <div>
+                    </div>
+                </div>
+            </div>
+           
+
+</body>
+
+</html>
